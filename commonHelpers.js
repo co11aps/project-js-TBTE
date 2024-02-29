815168578ec252e82b66eb05340f809463ae93be
co11aps <143988142+co11aps@users.noreply.github.com>
2024-02-29 18:43:10 +0000

    Deploying to gh-pages from @ co11aps/project-js-TBTE@f2b1b7981c5063c07beb9eb07ffab944c65f93ff 🚀

--- commonHelpers.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-bdcbc13b.js";import"./assets/vendor-10fca05e.js";const i={homeLinc:document.querySelector("a.home-link"),shoppingListLinc:document.querySelector("a.shopping-list-link")};switch(window.location.href){case i.homeLinc.href:i.homeLinc.classList.add("active-link");break;case i.shoppingListLinc.href:i.shoppingListLinc.classList.add("active-link");break}
-//# sourceMappingURL=commonHelpers.js.map
