08046f4c2916bf16636dee971a804c5d3d2e5131
pxlmady <144046046+pxlmady@users.noreply.github.com>
2024-03-01 12:19:22 +0200

    add handleBookInStorage

--- src/js/storage.js
+++ src/js/storage.js
@@ -7,3 +7,30 @@ function addToStorage(book) {
   shoppingList.push(book);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(shoppingList));
 }
+
+function handleBookInStorage(data) {
+  const isBookId = shoppingList.find(
+    bookInStorage => bookInStorage._id === data._id
+  );
+
+  if (isBookId) {
+    const dataBookID = refs.addBtnEL.getAttribute('data_id_of_book');
+
+    const bookIndex = shoppingList.findIndex(
+      bookInStorage => bookInStorage._id === dataBookID
+    );
+
+    shoppingList.splice(bookIndex, 1);
+    localStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify(shoppingList)
+    );
+    refs.addBtnEL.textContent = 'Add to shopping list';
+    modalMessage.remove();
+    return;
+  }
+
+  addToStorage(data);
+  refs.addBtnEL.textContent = 'Remove from the shopping list';
+  refs.addBtnEL.after(modalMessage);
+}  
