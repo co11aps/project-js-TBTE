a8948e5cfd82c03731230c5a757ca828fd62d662
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-01 08:43:23 +0000

    Deploying to gh-pages from @ co11aps/project-js-TBTE@bab83ad1fa49579bb4b6251f9c6a5c97c879d328 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import"./assets/active-page-link-5e7c9dee.js";import{a as l}from"./assets/vendor-4af391ac.js";const c="https://books-backend.p.goit.global/books";async function i(){const o=`${c}/top-books`;return(await l.get(o)).data}let e;const k=document.querySelector(".main-content");function u({_id:o,author:t,contributor:s,title:a,book_image:n,list_name:r}){return e=r,`<li class="book-item" id="${o}">
+import"./assets/active-page-link-5e7c9dee.js";import{a as l}from"./assets/vendor-4af391ac.js";const i="https://books-backend.p.goit.global/books";async function c(){const o=`${i}/top-books`;return(await l.get(o)).data}let e;const k=document.querySelector(".main-content");function u({_id:o,author:t,contributor:s,title:a,book_image:n,list_name:r}){return e=r,`<li class="book-item" id="${o}">
                 <div class="book-wrapper">
                 <img class="book-img" src="${n}" alt="${a} ${s}">
                 </div>
@@ -10,5 +10,5 @@ import"./assets/active-page-link-5e7c9dee.js";import{a as l}from"./assets/vendor
                     <h3 class="book-category-title">${e}</h3>
                     <ul class="books-category-list">`+t+`</ul>
                     <button class="button see-more-btn" data-category-home="${e}">See More</button>
-                </li>`}function m(o){return'<h2 class="books-home-title">Best Sellers <span class="books-home-title-span">Books</span></h2><ul class="books-home-list">'+o.map(p).join("")+"</ul>"}function b(o){const t=m(o);k.innerHTML=t}const d=i();b(d);
+                </li>`}function m(o){return'<h2 class="books-home-title">Best Sellers <span class="books-home-title-span">Books</span></h2><ul class="books-home-list">'+o.map(p).join("")+"</ul>"}function b(o){const t=m(o);k.innerHTML=t}async function d(){const o=await c();b(o)}d();
 //# sourceMappingURL=commonHelpers.js.map
