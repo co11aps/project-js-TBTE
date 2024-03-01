59145dd3a8747046497d5a9308c59345a50cf41f
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-01 20:05:24 +0800

    Update category_home.js

--- src/js/category_home.js
+++ src/js/category_home.js
@@ -1,15 +1,15 @@
-
 const categoriesList = document.querySelector('.categories-list');
 
-function addMarkup({list_name}) {
+function addMarkup({ list_name }) {
   return `<li class="category-item">
           <a href="${list_name}" class="category-link">${list_name}</a>
           </li>`;
 }
 
 export function createCategoriesList(data) {
-    console.log(data);
   const markup = data.map(addMarkup).join('');
-  const result = `<li class="category-item js-current-category" data-category="all categories"><a href="./index.html" class="category-link">All categories</a> </li>` + markup;
+  const result =
+    `<li class="category-item js-current-category" data-category="all categories"><a href="./index.html" class="category-link">All categories</a> </li>` +
+    markup;
   categoriesList.innerHTML = result;
 }
