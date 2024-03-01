9634e880f7d436f3e8e2f283697662bda4cd0e15
SerhiiBasok <sergeybsk19@gmail.com>
2024-03-02 00:39:37 +0200

    Update shopping-list.js
    Пункт 30 по ТЗ, чекаю на правки

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,28 +1,40 @@
-import refs from './refs';
+import refs from './js/refs';
 
-// Витягуємо дані з Local Storage
-const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
-
-// Функція для відображення повідомлення про відсутність книги
-function showEmptyMessage() {
-  const mainList = document.querySelector('.main-list');
-  const emptyMessageMarkup = `
-    <div class="empty-message">
-      <p>No books in the shopping list</p>
-      <img src="images/books-stack-mobile.png" alt="Empty Book Stack" />
-    </div>
-  `;
-  mainList.innerHTML = emptyMessageMarkup;
-}
-// Функція для відображення книг на сторінці
-function renderShoppingList() {
-  const mainList = document.querySelector('.main-list');
-
-  // Перевіряємо, чи є книги у списку
-  if (shoppingList.length === 0) {
-    showEmptyMessage();
+document.addEventListener('DOMContentLoaded', function () {
+  const shoppingListContainer = document.getElementById(
+    'shopping-list-container'
+  );
+  if (!shoppingListContainer) {
+    console.error('Shopping list container not found.');
     return;
   }
-}
 
-renderShoppingList();
+  const STORAGE_KEY = 'storage-of-books';
+  const storedBooks = JSON.parse(localStorage.getItem(STORAGE_KEY)) || []; // Отримуємо дані з localStorage || створюємо порожній масив, якщо дані відсутні
+
+  //Перевірка наявності книг
+  if (storedBooks.length > 0) {
+    renderBooksList(storedBooks);
+  } else {
+    showNoBooksMessage();
+  }
+
+  // перетворення масиву книг у рядок
+  function renderBooksList(books) {
+    refs.shoppingListContainer.innerHTML = books
+      .map(book => `<div>${book.title}</div>`)
+      .join('');
+  }
+
+  //повідомлення про відображення книг в Local Storage
+  function showNoBooksMessage() {
+    const emptyMessageContainer = document.createElement('div');
+    emptyMessageContainer.classList.add('empty-message');
+    emptyMessageContainer.innerHTML = `
+    <p>This page is empty, add some books and proceed to order.</p>
+    <img src="images/books-stack-mobile.png" alt="Empty Book Stack" />
+  `;
+
+    refs.shoppingListContainer.prepend(emptyMessageContainer);
+  }
+});
