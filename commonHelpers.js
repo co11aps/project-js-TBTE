752cf5b925d959b3aa9673cb6e086c00be97c7f2
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-01 12:05:46 +0000

    Deploying to gh-pages from @ co11aps/project-js-TBTE@59145dd3a8747046497d5a9308c59345a50cf41f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -12,5 +12,5 @@ import"./assets/active-page-link-3e1ebfed.js";import{a as n}from"./assets/vendor
                     <button class="button see-more-btn" data-category-home="${s}">See More</button>
                 </li>`}function g(o){return'<h2 class="books-home-title">Best Sellers <span class="books-home-title-span">Books</span></h2><ul class="books-home-list">'+o.map(m).join("")+"</ul>"}function b(o){const t=g(o);k.innerHTML=t}const d=document.querySelector(".categories-list");function y({list_name:o}){return`<li class="category-item">
           <a href="${o}" class="category-link">${o}</a>
-          </li>`}function f(o){console.log(o);const e='<li class="category-item js-current-category" data-category="all categories"><a href="./index.html" class="category-link">All categories</a> </li>'+o.map(y).join("");d.innerHTML=e}async function h(){const o=await u();b(o)}async function $(){const o=await l();f(o)}$();h();
+          </li>`}function f(o){const e='<li class="category-item js-current-category" data-category="all categories"><a href="./index.html" class="category-link">All categories</a> </li>'+o.map(y).join("");d.innerHTML=e}async function h(){const o=await u();b(o)}async function $(){const o=await l();f(o)}$();h();
 //# sourceMappingURL=commonHelpers.js.map
