150f6d031adf42580f1ddb7fb92cfa1c5cf862e6
NataliaGurova <nyxnata@ukr.net>
2024-03-01 15:45:44 +0200

    header

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,37 @@
+const openBtn = document.querySelector(".burger-menu-btn");
+const closeBtn = document.querySelector(".x-cross-menu-btn");
+const menu = document.querySelector(".backdrop-menu");
+
+
+
+
+openBtn.addEventListener("click", openMenu);
+closeBtn.addEventListener("click", closeMenu);
+
+ function openMenu() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+   menu.classList.toggle("is-open");
+   openBtn.classList.toggle("is-close");
+   closeBtn.classList.toggle("is-open");
+}
+  
+function closeMenu() {
+  menu.classList.toggle("is-open");
+   openBtn.classList.toggle("is-close");
+   closeBtn.classList.toggle("is-open");
+}
+
+// function closeMenuBtn() {
+
+//   openBtn.style.display = "block";
+//   closeBtn.style.display = "none";
+//   menu.style.display = "none"; 
+// };
+
+// function openMenuBtn() {
+
+//   openBtn.style.display = "none";
+//   closeBtn.style.display = "block";
+ 
+//   menu.style.display = "flex";
+// };
