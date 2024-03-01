eb8e7a03c0b50a5e360a68694f22c78e7c344238
HardcoreMushroom <ievhenii.gorbatiuk@gmail.com>
2024-03-01 05:29:54 +0200

    homepage-books

---
+++ src/js/block-home-books.js
@@ -0,0 +1,47 @@
+// import {getPopularBooks} from './books-api';
+// const data = await getPopularBooks();
+let bookCategory;
+const mainContainer = document.querySelector(".main-content");
+
+function templateBookCard({_id, author, contributor, title, book_image, list_name}){
+    bookCategory = list_name;
+    return `<li class="book-item" id="${_id}">
+                <div class="book-wrapper">
+                <img class="book-img" src="${book_image}" alt="${title} ${contributor}">
+                </div>
+                <div class="book-info">
+                <p class="book-info-title">${title}</p>
+                <p class="book-info-author">${author}</p>
+                </div>
+            </li>`;
+}
+
+function templateBookCategoryEl({books}){
+
+    const bookCards = books.map(templateBookCard).join("");
+    const result = `<li class="book-item-home">
+                    <h3 class="book-category-title">${bookCategory}</h3>
+                    <ul class="books-category-list">` + bookCards +
+                    `</ul>
+                    <button class="button see-more-btn" data-category-home="${bookCategory}">See More</button>
+                </li>`;
+
+    return result;
+}
+
+
+function templateHomeBookMarkup(dataPopularBooks){
+    const markup = dataPopularBooks.map(templateBookCategoryEl).join("");
+    const result = `<h2 class="books-home-title">Best Sellers <span class="books-home-title-span">Books</span></h2>` + 
+                 `<ul class="books-home-list">` + markup + `</ul>`;
+
+    return result;
+}
+
+
+export function renderHomeBooksMarkup(dataPopularBooks){
+    const markup = templateHomeBookMarkup(dataPopularBooks);
+    console.log(dataPopularBooks);
+    mainContainer.insertAdjacentHTML('beforeend', markup);
+}
+// renderHomeBooksMarkup(data);
