b8686390f1ac1f068e4c8d55b08bdab9d7811785
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-03 19:24:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -29,8 +29,6 @@ import './js/selected_category.js';
 import { categoriesList } from './js/category_home.js';
 import { onCategoryClick } from './js/selected_category.js';
 
-import { createCategoriesList } from './js/category_home.js';
-
 async function renderMainPageBookList() {
   loaderOn();
   const data = await getPopularBooks();
