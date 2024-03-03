50206bd37a08d3c5e197966f7ad622330e305d9b
SerhiiBasok <sergeybsk19@gmail.com>
2024-03-04 01:10:48 +0200

    changeJS/CSS
    Додав функцію видалення книжки LocalStorage до storage.js. На локал хості видаляє "костилі які додав в ручну" Змінив шлях до картинки про відображення відсутності книг і застилізував під масштабування. Пробував додати стилі до шаблону книги, але це потрібно робити безпосередньо коли в Shoping List будуть додаватися книги з Local Storage(в мене не вийшло з костильним обʼєктом в локал хост)

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,36 +1,40 @@
 export const STORAGE_KEY = 'storage-of-books';
 
-export const shoppingList =
-  JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+export const shoppingList = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+
+export function removeBook(bookId) {
+  const updatedShoppingList = shoppingList.filter(book => book._id !== bookId);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedShoppingList));
+  return updatedShoppingList;
+}
 
 export function addToStorage(book) {
   shoppingList.push(book);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(shoppingList));
 }
-
 export function handleBookInStorage(data) {
   const isBookId = shoppingList.find(
     bookInStorage => bookInStorage._id === data._id
   );
 
   if (isBookId) {
-    const dataBookID = document.querySelector('.modal-pop-up-btn').getAttribute('data_id_of_book');
-
+    const dataBookID = document
+      .querySelector('.modal-pop-up-btn')
+      .getAttribute('data_id_of_book');
     const bookIndex = shoppingList.findIndex(
       bookInStorage => bookInStorage._id === dataBookID
     );
 
     shoppingList.splice(bookIndex, 1);
-    localStorage.setItem(
-      STORAGE_KEY,
-      JSON.stringify(shoppingList)
-    );
-    document.querySelector('.modal-pop-up-btn').textContent = 'Add to shopping list';
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(shoppingList));
+    document.querySelector('.modal-pop-up-btn').textContent =
+      'Add to shopping list';
     modalMessage.remove();
     return;
   }
 
   addToStorage(data);
-  document.querySelector('.modal-pop-up-btn').textContent = 'Remove from the shopping list';
+  document.querySelector('.modal-pop-up-btn').textContent =
+    'Remove from the shopping list';
   document.querySelector('.modal-pop-up-btn').after(modalMessage);
-}  
+}
