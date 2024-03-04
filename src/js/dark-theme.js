c9182b350e7058ff394e143d70175f8c9fd00fa0
PuDDDle <144221380+PuDDDle@users.noreply.github.com>
2024-03-04 16:56:26 +0200

    Update dark-theme.js

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,16 +1,15 @@
-const iconUse = document.querySelector(".header-use-logo");
+const iconUse = document.querySelector('.header-use-logo');
 
 console.log(iconUse);
 
- // function to set a given theme/color-scheme
-        function setTheme(themeName) {
-            localStorage.setItem('theme', themeName);
-            document.documentElement.className = themeName;
-            console.log(themeName);
-        }
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+  console.log(themeName);
+}
 
-
- function toggleTheme() {
+function toggleTheme() {
   const theme = localStorage.getItem('theme');
   if (theme === 'dark') {
     setTheme('light');
@@ -19,23 +18,18 @@ console.log(iconUse);
   }
 }
 
-
-
 document.getElementById('slider').addEventListener('change', toggleTheme);
 
-
-
-      function checkboxCheck() {
-            if (localStorage.getItem('theme') === 'dark') {
-                setTheme('dark');
-              document.getElementById("slider").checked = true;
-              // iconUse.setAttribute('href', './images/icons.svg#icon-logo-dark');
-              console.dir(iconUse.getAttribute('href'));
-            } else {
-                setTheme('light');
-              document.getElementById("slider").checked = false;
-              // iconUse.setAttribute('href', './images/icons.svg#icon-logo');
-            }
+function checkboxCheck() {
+  if (localStorage.getItem('theme') === 'dark') {
+    setTheme('dark');
+    document.getElementById('slider').checked = true;
+    // iconUse.setAttribute('href', './images/icons.svg#icon-logo-dark');
+    console.dir(iconUse.getAttribute('href'));
+  } else {
+    setTheme('light');
+    document.getElementById('slider').checked = false;
+    // iconUse.setAttribute('href', './images/icons.svg#icon-logo');
+  }
 }
- checkboxCheck()
-
+checkboxCheck();
