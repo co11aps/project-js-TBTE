f8c0544471004c6be2ce7c1bbac5f1b0cff9d895
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-04 13:58:01 +0000

    Deploying to gh-pages from @ co11aps/project-js-TBTE@7cf238481e38b2d2d036e881d15cc772b41b769c 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,14 +1,14 @@
-import"./assets/mobile-menu-295aee8c.js";const l="storage-of-books";JSON.parse(localStorage.getItem(l));const c="storage-of-books",a=document.querySelector(".shopping-list"),r=document.createElement("div");r.classList.add("empty-message");r.innerHTML=`
+import"./assets/mobile-menu-295aee8c.js";const d="storage-of-books";JSON.parse(localStorage.getItem(d));const g="/project-js-TBTE/assets/books-stack-mobile-487658af.png",c="/project-js-TBTE/assets/books-stack-a8e7c57b.png",i="storage-of-books",a=document.querySelector(".shopping-list"),r=document.createElement("div");r.classList.add("empty-message");r.innerHTML=`
   <p>This page is empty, add some books and proceed to order.</p>
-<img 
-  class="book-stack-image"
-  srcset="/images/books-stack-mobile.png 767w, 
-          /images/books-stack.png 768w" 
-  sizes="(max-width: 767px) 265px, 100vw" 
-  src="/images/books-stack-mobile.png" 
-  alt="Empty Book Stack"  
-/>
-`;async function i(){try{const o=await new Promise((e,t)=>{const s=localStorage.getItem(c);e(s?JSON.parse(s):[])});return console.log("Stored books:",o),o}catch(o){throw console.error("Error getting stored books:",o),o}}async function n(){try{const o=await i();a.innerHTML="",Array.isArray(o)&&o.length>0?o.forEach(e=>{const t=document.createElement("li");t.appendChild(d(e)),a.appendChild(t)}):a.appendChild(r)}catch(o){console.error("Error rendering books:",o)}}function d(o){const e=document.createElement("div");return e.classList.add("book-card"),e.innerHTML=`
+  <img 
+    class="book-stack-image"
+    srcset="${g} 767w, 
+            ${c} 768w" 
+    sizes="(max-width: 767px) 265px, 100vw" 
+    src="${c}" 
+    alt="Empty Book Stack"  
+  />
+`;async function n(){try{return await new Promise((e,t)=>{const s=localStorage.getItem(i);e(s?JSON.parse(s):[])})}catch(o){throw console.error("Error getting stored books:",o),o}}async function l(){try{const o=await n();a.innerHTML="",Array.isArray(o)&&o.length>0?o.forEach(e=>{const t=document.createElement("li");t.appendChild(m(e)),a.appendChild(t)}):a.appendChild(r)}catch(o){console.error("Error rendering books:",o)}}function m(o){const e=document.createElement("div");return e.classList.add("book-card"),e.innerHTML=`
     <ul class="card-list">
       <li class="card-book">
         <div class="information-card">
@@ -37,5 +37,5 @@ import"./assets/mobile-menu-295aee8c.js";const l="storage-of-books";JSON.parse(l
         </div>
       </li>
     </ul>
-  `,e}async function g(o){try{const t=(await i()).filter(s=>s._id!=o);localStorage.setItem(c,JSON.stringify(t)),n(t)}catch(e){console.error("Error removing book from localStorage:",e)}}const m=document.querySelector(".shopping-list");m.addEventListener("click",async o=>{const e=o.target.closest(".remove-book-btn");if(e){const t=e.getAttribute("data-book-id");console.log("Remove button clicked for book ID:",t),await g(t)}});n();
+  `,e}async function k(o){try{const t=(await n()).filter(s=>s._id!=o);localStorage.setItem(i,JSON.stringify(t)),l(t)}catch(e){console.error("Error removing book from localStorage:",e)}}const p=document.querySelector(".shopping-list");p.addEventListener("click",async o=>{const e=o.target.closest(".remove-book-btn");if(e){const t=e.getAttribute("data-book-id");console.log("Remove button clicked for book ID:",t),await k(t)}});l();
 //# sourceMappingURL=commonHelpers2.js.map
