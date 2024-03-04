8dd3f667cf266918c759dd4485a55e8d4be433d7
PuDDDle <144221380+PuDDDle@users.noreply.github.com>
2024-03-04 16:34:39 +0200

    update ModalClose

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -1,39 +1,37 @@
 import { getBookInfo } from './books-api';
 import { showError } from './block-home-books';
-const booksContainer = document.querySelector(".main-content");
-const modalWindow = document.querySelector(".backdrop-pop-up");
-const modalWindowContent = document.querySelector(".modal-pop-up-content");
+import { modalClose } from './modal';
+const booksContainer = document.querySelector('.main-content');
+const modalWindow = document.querySelector('.backdrop-pop-up');
+const modalWindowContent = document.querySelector('.modal-pop-up-content');
 const body = document.querySelector('body');
 let modalBookid;
-booksContainer.addEventListener("click", onBooksContainerClick);
+booksContainer.addEventListener('click', onBooksContainerClick);
 
-
-async function onBooksContainerClick(e){
-    modalBookid = e.target.closest(".book-item").id;
-    if(modalBookid){
-        modalWindow.classList.remove("is-hidden");
-        body.classList.add("no-scroll");
-        try{
-            let bookInfo = await getBookInfo(modalBookid);
-            console.log(bookInfo);
-            renderBookInfoModal(bookInfo);
-        }catch(err){
-            showError(err);
-        }
+async function onBooksContainerClick(e) {
+  modalBookid = e.target.closest('.book-item').id;
+  if (modalBookid) {
+    modalWindow.classList.remove('is-hidden');
+    modalClose();
+    try {
+      let bookInfo = await getBookInfo(modalBookid);
+      renderBookInfoModal(bookInfo);
+    } catch (err) {
+      showError(err);
     }
-
+  }
 }
 
 function templateBookInfoModal({
-    book_image,
-    contributor,
-    description,
-    _id,
-    title,
-    author,
-    buy_links,
-}){
-    return `<img class="modal-img" src="${book_image}" alt="${title} ${contributor}" id="${_id}">
+  book_image,
+  contributor,
+  description,
+  _id,
+  title,
+  author,
+  buy_links,
+}) {
+  return `<img class="modal-img" src="${book_image}" alt="${title} ${contributor}" id="${_id}">
             <div class="modal-img-description">
                 <h2 class="modal-book-title">${title}</h2>
                 <h3 class="modal-book-author">${author}</h3>
@@ -49,9 +47,9 @@ function templateBookInfoModal({
                             <img class="book-shop" src="../images/apple-books-logo.png" alt="Shop Logo">
                         </a>
                 </ul>
-            </div>`
+            </div>`;
 }
-function renderBookInfoModal(data){
-    const markup = templateBookInfoModal(data);
-    modalWindowContent.insertAdjacentHTML("afterbegin", markup);
+function renderBookInfoModal(data) {
+  const markup = templateBookInfoModal(data);
+  modalWindowContent.insertAdjacentHTML('afterbegin', markup);
 }
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,4 @@
-(() => {
+export const modalClose = () => {
   const refsModal = {
     openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
@@ -9,20 +9,13 @@
     element.addEventListener('click', toggleModal);
   });
 
-  refsModal.closeModalBtn.addEventListener('click', closeAndEnableScroll);
-
+  refsModal.closeModalBtn.addEventListener('click', closeModal);
   refsModal.modal.addEventListener('click', removeModal);
-
-  document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape') {
-      closeAndEnableScroll();
-    }
-  });
+  document.addEventListener('keydown', onEscapeKey);
 
   function removeModal(e) {
     if (e.target === e.currentTarget) {
-      closeAndEnableScroll();
-      removeEventListeners();
+      closeModal();
     }
   }
 
@@ -30,41 +23,39 @@
     refsModal.modal.classList.toggle('is-hidden');
     refsModal.modal.classList.toggle('is-open');
 
-    if (refsModal.modal.classList.contains('is-open')) {
-      disableBodyScroll();
+    if (refsModal.modal.classList.contains('is-hidden')) {
+      document.body.classList.add('no-scroll');
+      document.addEventListener('keydown', onEscapeKey);
+      refsModal.closeModalBtn.addEventListener('click', closeModal);
+      refsModal.modal.addEventListener('click', removeModal);
     } else {
       enableBodyScroll();
+      removeEventListeners();
     }
   }
 
-  function disableBodyScroll() {
-    document.body.classList.add('no-scroll');
+  function closeModal() {
+    refsModal.modal.classList.add('is-hidden');
+    enableBodyScroll();
+    removeEventListeners();
+  }
+
+  function onEscapeKey(e) {
+    if (e.key === 'Escape') {
+      closeModal();
+    }
   }
 
   function enableBodyScroll() {
     document.body.classList.remove('no-scroll');
   }
 
-  function closeAndEnableScroll() {
-    refsModal.modal.classList.add('is-hidden');
-    enableBodyScroll();
-    removeEventListeners();
-  }
-
   function removeEventListeners() {
     refsModal.openModalBtn.forEach(element => {
       element.removeEventListener('click', toggleModal);
     });
-
-    refsModal.closeModalBtn.removeEventListener('click', closeAndEnableScroll);
+    refsModal.closeModalBtn.removeEventListener('click', closeModal);
     refsModal.modal.removeEventListener('click', removeModal);
     document.removeEventListener('keydown', onEscapeKey);
   }
-
-  function onEscapeKey(e) {
-    if (e.key === 'Escape') {
-      closeAndEnableScroll();
-      removeEventListeners();
-    }
-  }
-})();
+};
