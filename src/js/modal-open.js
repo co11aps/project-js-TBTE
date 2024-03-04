e66b22299b74b6019d45c54bf52f806c62176116
PuDDDle <144221380+PuDDDle@users.noreply.github.com>
2024-03-04 17:23:41 +0200

    update modal close

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -3,41 +3,39 @@ import { showError } from './block-home-books';
 import amazonLogo from '../images/amazon-logo.png';
 import appleBooksLogo from '../images/apple-books-logo.png';
 
-const booksContainer = document.querySelector(".main-content");
-const modalWindow = document.querySelector(".backdrop-pop-up");
-const modalWindowContent = document.querySelector(".modal-pop-up-content");
+const booksContainer = document.querySelector('.main-content');
+const modalWindow = document.querySelector('.backdrop-pop-up');
+const modalWindowContent = document.querySelector('.modal-pop-up-content');
 const body = document.querySelector('body');
 let modalBookEl;
-booksContainer.addEventListener("click", onBooksContainerClick);
+booksContainer.addEventListener('click', onBooksContainerClick);
 
-
-async function onBooksContainerClick(e){
-    modalBookEl = e.target.closest(".book-item");
-    modalWindowContent.innerHTML = "";
-    if(modalBookEl){
-        modalWindow.classList.remove("is-hidden");
-        body.classList.add("no-scroll");
-        try{
-            let modalBookId = modalBookEl.id;
-            let bookInfo = await getBookInfo(modalBookId);
-            renderBookInfoModal(bookInfo);
-        }catch(err){
-            showError(err);
-        }
+async function onBooksContainerClick(e) {
+  modalBookEl = e.target.closest('.book-item');
+  modalWindowContent.innerHTML = '';
+  if (modalBookEl) {
+    modalWindow.classList.remove('is-hidden');
+    modalClose();
+    try {
+      let modalBookId = modalBookEl.id;
+      let bookInfo = await getBookInfo(modalBookId);
+      renderBookInfoModal(bookInfo);
+    } catch (err) {
+      showError(err);
     }
-
+  }
 }
 
 function templateBookInfoModal({
-    book_image,
-    contributor,
-    description,
-    _id,
-    title,
-    author,
-    buy_links,
-}){
-    return `<img class="modal-img" src="${book_image}" alt="${title} ${contributor}" id="${_id}">
+  book_image,
+  contributor,
+  description,
+  _id,
+  title,
+  author,
+  buy_links,
+}) {
+  return `<img class="modal-img" src="${book_image}" alt="${title} ${contributor}" id="${_id}">
             <div class="modal-img-description">
                 <h2 class="modal-book-title">${title}</h2>
                 <h3 class="modal-book-author">${author}</h3>
@@ -53,9 +51,9 @@ function templateBookInfoModal({
                             <img class="book-shop" src="${appleBooksLogo}" alt="Shop Logo">
                         </a>
                 </ul>
-            </div>`
+            </div>`;
 }
-function renderBookInfoModal(data){
-    const markup = templateBookInfoModal(data);
-    modalWindowContent.insertAdjacentHTML("afterbegin", markup);
+function renderBookInfoModal(data) {
+  const markup = templateBookInfoModal(data);
+  modalWindowContent.insertAdjacentHTML('afterbegin', markup);
 }
