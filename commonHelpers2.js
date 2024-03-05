a706f9e71c4887000dc96c91bab83ecd0abf0406
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-05 03:01:41 +0000

    Deploying to gh-pages from @ co11aps/project-js-TBTE@91aefc7f3dbc3921d09dc7533bead68ac56dc9d3 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,41 +1,38 @@
-import"./assets/dark-theme-4fd7b0b0.js";const d="storage-of-books";JSON.parse(localStorage.getItem(d));const g="/project-js-TBTE/assets/books-stack-mobile-487658af.png",c="/project-js-TBTE/assets/books-stack-a8e7c57b.png",i="storage-of-books",a=document.querySelector(".shopping-list"),r=document.createElement("div");r.classList.add("empty-message");r.innerHTML=`
+import"./assets/dark-theme-f5a77cb3.js";const u="storage-of-books";JSON.parse(localStorage.getItem(u));const y="/project-js-TBTE/assets/books-stack-mobile-487658af.png",l="/project-js-TBTE/assets/books-stack-a8e7c57b.png",d="storage-of-books",c=document.querySelector(".shopping-list"),n=document.createElement("div");n.classList.add("empty-message");n.innerHTML=`
   <p>This page is empty, add some books and proceed to order.</p>
   <img 
     class="book-stack-image"
-    srcset="${g} 767w, 
-            ${c} 768w" 
+    srcset="${y} 767w, 
+            ${l} 768w" 
     sizes="(max-width: 767px) 265px, 100vw" 
-    src="${c}" 
+    src="${l}" 
     alt="Empty Book Stack"  
   />
-`;async function n(){try{return await new Promise((e,t)=>{const s=localStorage.getItem(i);e(s?JSON.parse(s):[])})}catch(o){throw console.error("Error getting stored books:",o),o}}async function l(){try{const o=await n();a.innerHTML="",Array.isArray(o)&&o.length>0?o.forEach(e=>{const t=document.createElement("li");t.appendChild(m(e)),a.appendChild(t)}):a.appendChild(r)}catch(o){console.error("Error rendering books:",o)}}function m(o){const e=document.createElement("div");return e.classList.add("book-card"),e.innerHTML=`
-    <ul class="card-list">
-      <li class="card-book">
-        <div class="information-card">
-          <div>
-            <img class="book-cover" src="${o.image}" alt="${o.title}" />
-          </div>
-          <div class="data-or-books-container" >
-            <h2 class="book-title">${o.title}</h2>
-            <h3 class="book-category">${o.bookCategory}</h3>
-            <p class="book-description">${o.title}</p>
-            <p class="book-author">${o.author}</p>
-            <ul class="market-place-list">
-              <li class="market-link" width="32" height="11">
-                <img class="market-image" src="/images/amazon-logo.png" alt="Marketplace-Image">
-              </li>
-              <li class="market-link" width="16" height="16">
-                <img class="market-image" src="/images/apple-books-logo.png" alt="Marketplace-Image">
-              </li>
-            </ul>
-            <button class="remove-book-btn" data-book-id="${o._id}">
-              <svg width="38" height="38" class="remove-icon">
-                <use href="/images/icons.svg#icon-dump"></use>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </li>
-    </ul>
-  `,e}async function k(o){try{const t=(await n()).filter(s=>s._id!=o);localStorage.setItem(i,JSON.stringify(t)),l(t)}catch(e){console.error("Error removing book from localStorage:",e)}}const p=document.querySelector(".shopping-list");p.addEventListener("click",async o=>{const e=o.target.closest(".remove-book-btn");if(e){const t=e.getAttribute("data-book-id");console.log("Remove button clicked for book ID:",t),await k(t)}});l();
+`;async function k(){try{return await new Promise((s,e)=>{const t=localStorage.getItem(d);s(t?JSON.parse(t):[])})}catch(o){throw console.error("Error getting stored books:",o),o}}async function g(){try{const o=await k();c.innerHTML="",Array.isArray(o)&&o.length>0?o.forEach(s=>{const e=document.createElement("li");e.appendChild(v(s)),c.appendChild(e)}):c.appendChild(n)}catch(o){console.error("Error rendering books:",o)}}function v(o){const{title:s,category:e,description:t,author:p,_id:i,buy_links:r,book_image:m}=o,a=document.createElement("div");return a.classList.add("book-card"),a.innerHTML=`
+    <div class="card-book">
+      <img
+        class="book-cover"
+        src="${m}"
+        alt="${s}"
+        id="${i}"
+      />
+      <div class="shopping-list-img-description">
+        <h2 class="book-title">${s}</h2>
+        <h3 class="book-category">${e}</h3>
+        <p class="book-description">${t}</p>
+        <p class="book-author">${p}</p>
+         <ul class="storage-shops">
+          ${r&&Array.isArray(r)?r.map((b,h)=>`
+              <li class="list-shop">
+                <a href="${b.url}" target="_blank" class="modal-shop-link">
+                  <img class="list-book-shop" src="${h===0?amazonLogo:appleBooksLogo}" alt="Shop Logo" /></a>
+              </li>`).join(""):""} </ul>
+        <button class="remove-book-btn" data-book-id="${i}">
+          <svg width="38" height="38" class="remove-icon">
+            <use href="/images/icons.svg#icon-dump"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+  `,a}async function f(o){try{const e=(await k()).filter(t=>t._id!=o);localStorage.setItem(d,JSON.stringify(e)),g(e)}catch(s){console.error("Error removing book from localStorage:",s)}}const S=document.querySelector(".shopping-list");S.addEventListener("click",async o=>{const s=o.target.closest(".remove-book-btn");if(s){const e=s.getAttribute("data-book-id");console.log("Remove button clicked for book ID:",e),await f(e)}});g();
 //# sourceMappingURL=commonHelpers2.js.map
