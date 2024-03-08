dadaf88c4035290ca12ee93dc9e05738b4e99773
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-08 16:51:31 +0000

    Deploying to gh-pages from @ co11aps/project-js-TBTE@7c717bfbb12ca4eab8278170c5255ffee806fbb3 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,39 +1,39 @@
-import{a as y,b as S}from"./assets/apple-books-logo-f870ee45.js";import{P as v}from"./assets/vendor-30319b8a.js";const B="/project-js-TBTE/assets/books-stack-mobile-487658af.png",h="/project-js-TBTE/assets/books-stack-a8e7c57b.png",L=document.querySelector("#pagination"),p="storage-of-books",g=document.querySelector(".shopping-list"),m=document.createElement("li");m.classList.add("empty-message");m.innerHTML=`
+import{a as b,b as h}from"./assets/apple-books-logo-126831ae.js";import"./assets/vendor-722ab571.js";const u="/project-js-TBTE/assets/books-stack-mobile-487658af.png",l="/project-js-TBTE/assets/books-stack-a8e7c57b.png",y="/project-js-TBTE/assets/icons-5a87309a.svg",d="storage-of-books",r=document.querySelector(".shopping-list"),c=document.createElement("li");c.classList.add("empty-message");c.innerHTML=`
   <p>This page is empty, add some books and proceed to order.</p>
   <img 
     class="book-stack-image"
-    srcset="${B} 767w, 
-            ${h} 768w" 
+    srcset="${u} 767w, 
+            ${l} 768w" 
     sizes="(max-width: 767px) 265px, 100vw" 
-    src="${h}" 
+    src="${l}" 
     alt="Empty Book Stack"  
   />
-`;async function b(){try{return await new Promise((o,t)=>{const s=localStorage.getItem(p);o(s?JSON.parse(s):[])})}catch(e){throw console.error("Error getting stored books:",e),e}}function $(e){const{title:o,category:t,list_name:s,author:c,_id:l,buy_links:n,book_image:i}=e,d=document.createElement("div");return d.classList.add("book-card"),d.innerHTML=`
+`;async function p(){try{return await new Promise((s,t)=>{const e=localStorage.getItem(d);s(e?JSON.parse(e):[])})}catch(o){throw console.error("Error getting stored books:",o),o}}async function k(){try{const o=await p();r.innerHTML="",Array.isArray(o)&&o.length>0?o.forEach(s=>{const t=document.createElement("li");t.appendChild(B(s)),r.appendChild(t)}):r.appendChild(c)}catch(o){console.error("Error rendering books:",o)}}function B(o){const{title:s,list_name:t,description:e,author:g,_id:i,buy_links:n,book_image:m}=o,a=document.createElement("div");return a.classList.add("book-card"),a.innerHTML=`
     <div class="card-book">
-  <img class="book-cover" src="${i}" alt="${o}" id="${l}" />
+  <img class="book-cover" src="${m}" alt="${s}" id="${i}" />
   <div class="shopping-list-img-description">
-    <h2 class="book-title">${o}</h2>
+    <h2 class="book-title">${s}</h2>
     <h3 class="book-category">${t}</h3>
-    <p class="book-description">${s}</p>
-    <p class="book-author">${c}</p>
+    <p class="book-description">${e}</p>
+    <p class="book-author">${g}</p>
     <ul class="storage-shops">
       <li class="list-shop">
-        <a href="${n[0].url}" target="_blank" class="storage-shop-link">
-          <img class="amazon" width="32" height="11"  src="${y}" alt="Shop Logo" />
+        <a href="${n[0].url}" target="_blank" aria-label="Buy on Amazon" class="storage-shop-link">
+          <img class="amazon" width="32" height="11"  src="${b}" alt="Shop Logo" />
         </a>
       </li>
       <li class="list-shop">
-        <a href="${n[1].url}" target="_blank" class="storage-shop-link">
-          <img class="book-shop" width="16" height="16" src="${S}" alt="Shop Logo" />
+        <a href="${n[1].url}" target="_blank" aria-label="Buy in Apple books" class="storage-shop-link">
+          <img class="book-shop" width="16" height="16" src="${h}" alt="Shop Logo" />
         </a>
       </li>
     </ul>
-    <button class="remove-book-btn" data-book-id="${l}">
-      <svg width="38" height="38" class="remove-icon">
-        <use href="./images/icons.svg#icon-dump"></use>
+    <button aria-label="Delete book" type="button"class="remove-book-btn" data-book-id="${i}">
+      <svg width="28" height="28" class="remove-icon">
+        <use xlink:href="${y}#icon-dump"></use>
       </svg>
     </button>
   </div>
 </div>
-  `,d}function f(){const e=localStorage.getItem(p);if(e){const o=JSON.parse(e).length;return Number(o)}else return console.log("Значення відсутнє у локальному сховищі."),0}let a=f();console.log("Stored books length:",a);const r=new v(L,{totalItems:a,itemsPerPage:3,visiblePages:3,page:1});console.log(r);document.querySelector(".container-pagination").classList.add("is-hidden");async function k(e,o){try{const t=await b();if(g.innerHTML="",Array.isArray(t)&&t.length>0){console.log(t);const s=(o-1)*3,c=s+3;t.slice(s,c).forEach(n=>{const i=document.createElement("li");i.appendChild($(n)),g.appendChild(i)}),t.length>=4?document.querySelector(".container-pagination").classList.remove("is-hidden"):document.querySelector(".container-pagination").classList.add("is-hidden")}else g.appendChild(m),document.querySelector(".container-pagination").classList.add("is-hidden")}catch(t){console.error("Error rendering books:",t)}}function w(){r.reset(),a=f(),r.setTotalItems(a)}r.on("beforeMove",e=>{const o=e.page;k(a,o),console.log(o)});k(a,1);let u;async function E(e){try{u=(await b()).filter(t=>t._id!==e),localStorage.setItem(p,JSON.stringify(u)),w(),k(a,1)}catch(o){console.error("Error removing book from localStorage:",o)}}const T=document.querySelector(".shopping-list");T.addEventListener("click",async e=>{const o=e.target.closest(".remove-book-btn");if(o){const t=o.getAttribute("data-book-id");console.log("Remove button clicked for book ID:",t),await E(t),r.reset()}});
+  `,a}async function f(o){try{const t=(await p()).filter(e=>e._id!=o);localStorage.setItem(d,JSON.stringify(t)),k(t)}catch(s){console.error("Error removing book from localStorage:",s)}}const v=document.querySelector(".shopping-list");v.addEventListener("click",async o=>{const s=o.target.closest(".remove-book-btn");if(s){const t=s.getAttribute("data-book-id");await f(t)}});k();
 //# sourceMappingURL=commonHelpers2.js.map
