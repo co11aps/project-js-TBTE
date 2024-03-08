6dd1c0be0c73ad58031b6858b590463fbaf5a3d3
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-08 16:21:13 +0000

    Deploying to gh-pages from @ co11aps/project-js-TBTE@a1488dc1deda40d0d54d8478f175fedbb49b71f2 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{a as b,b as h}from"./assets/apple-books-logo-b9d7f1fd.js";import"./assets/vendor-722ab571.js";const u="/project-js-TBTE/assets/books-stack-mobile-487658af.png",l="/project-js-TBTE/assets/books-stack-a8e7c57b.png",y="/project-js-TBTE/assets/icons-5a87309a.svg",d="storage-of-books",r=document.querySelector(".shopping-list"),c=document.createElement("li");c.classList.add("empty-message");c.innerHTML=`
+import{a as b,b as h}from"./assets/apple-books-logo-126831ae.js";import"./assets/vendor-722ab571.js";const u="/project-js-TBTE/assets/books-stack-mobile-487658af.png",l="/project-js-TBTE/assets/books-stack-a8e7c57b.png",y="/project-js-TBTE/assets/icons-5a87309a.svg",d="storage-of-books",r=document.querySelector(".shopping-list"),c=document.createElement("li");c.classList.add("empty-message");c.innerHTML=`
   <p>This page is empty, add some books and proceed to order.</p>
   <img 
     class="book-stack-image"
