a1488dc1deda40d0d54d8478f175fedbb49b71f2
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-09 00:20:51 +0800

    weather vijet update

--- src/js/form-for-weather.js
+++ src/js/form-for-weather.js
@@ -4,6 +4,7 @@ import iziToast from 'izitoast';
 const form = document.querySelector('.js-search');
 const cityButton = document.querySelector('.search-city');
 const weatherInfo = document.getElementById('js-info-weather-city');
+const forecastText = document.querySelector('p.forecast-text');
 
 form.addEventListener('submit', getFormValues);
 
@@ -21,27 +22,15 @@ async function getFormValues(event) {
     const weatherHtml = `
                 <h2 class="country-city-title">${country}, ${city}</h2>
                 <img class="weather-city-image" src="${image}" alt="${text}">
-                <p class="weather-city-info">Temperature: ${Math.floor(
+                <p class="weather-city-info">Temp.: ${Math.floor(
                   temperature
                 )} &deg;C</p>
                 <p class="weather-city-info">${text}</p>
         `;
     weatherInfo.innerHTML = weatherHtml;
-    if (
-      temperature < 0 ||
-      text.toLowerCase().includes('cloudy') ||
-      text.toLowerCase().includes('rain')
-    ) {
-      iziToast.info({
-        title: 'Reading Weather',
-        message: 'The weather is perfect for reading a book!',
-        position: 'center',
-        timeout: 10000,
-        messageColor: 'blue',
-        backgroundColor: 'yellow',
-        messageSize: '32px',
-      });
-    }
+
+    forecastText.textContent = generateForecast(text);
+
     event.target.elements.city.value = '';
     cityButton.blur();
   } catch (error) {
@@ -65,4 +54,29 @@ async function serviceWeather(city) {
     })
   ).data;
   return response;
+}
+
+function generateForecast(text) {
+  const rain =
+    'Rain taps softly, like music, setting the scene for books. Rainy days are cozy, perfect for getting lost in books';
+  const snow =
+    'Snow falls gently, making reading by the fire even better. Snowy weather creates a peaceful setting for immersive reading sessions';
+  const clear =
+    'Sunny days make reading outside under trees delightful. Sunlight makes reading enjoyable with its warm and bright rays';
+  const anyWeather =
+    'Any weather is book weather! Embrace reading anytime, anywhere';
+  const cloudy =
+    'Soft light filters through the clouds, ideal for cozy reading. Cloudy days offer a calm atmosphere, perfect for uninterrupted reading';
+
+  if (text.toLowerCase().includes('cloudy')) {
+    return cloudy;
+  } else if (text.toLowerCase().includes('rain')) {
+    return rain;
+  } else if (text.toLowerCase().includes('snow')) {
+    return snow;
+  } else if (text.toLowerCase().includes('sunny')) {
+    return clear;
+  } else {
+    return anyWeather;
+  }
 }
