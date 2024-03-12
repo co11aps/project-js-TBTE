ef62ddba9b596efe9ee43a10e25316199410b081
co11aps <143988142+co11aps@users.noreply.github.com>
2024-03-12 06:24:47 +0000

    Deploying to gh-pages from @ co11aps/project-js-TBTE@a62a5e64eb58465f0569162706dcdfaa2e432598 🚀

---
+++ assets/config-905a1586.js
@@ -0,0 +1,2 @@
+const o={apiKey:"AIzaSyB41WCULKnNp2IiFVK82TtJH5XyzvwOUnY",authDomain:"goit-bookshelf-project.firebaseapp.com",projectId:"goit-bookshelf-project",storageBucket:"goit-bookshelf-project.appspot.com",messagingSenderId:"763025601632",appId:"1:763025601632:web:2a1d50db575e9c30caff05"};export{o as f};
+//# sourceMappingURL=config-905a1586.js.map
